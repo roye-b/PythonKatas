58c85c3b0632ba7be7305452a0511787c4cf5e88
roye <rbega2@shilav.com>
2024-09-13 00:27:52 -0700

    test1

--- katas/hello_world.py
+++ katas/hello_world.py
@@ -1,12 +1,8 @@
 def hello_world():
-    """
-    Returns the string "hello world".
-    """
-
-
+    return "Hello, World!"
 
 result_str = hello_world()
-print(result_str)   # hello world should be printed
+print(result_str)
 
 
 """
